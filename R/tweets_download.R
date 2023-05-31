98489428a973e8c2ff2c4b0ec898a8761594c39f
Gianluca Boo <gianluca.boo@gmail.com>
2023-05-31 10:06:33 +0200

    Update tweets_download.R

--- R/tweets_download.R
+++ R/tweets_download.R
@@ -1,2 +1,2 @@
 #browseURL("https://smallarmssurvey.shinyapps.io/afg_tweets_download/")
-browseURL("www.gazzetta.it", browser = getOption("browser"))
+browseURL("https://www.gazzetta.it", browser = getOption("browser"))
