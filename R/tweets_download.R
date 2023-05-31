964c8e5a645ec9fa07ee7e948faec7e4d331c02a
Gianluca Boo <gianluca.boo@gmail.com>
2023-05-31 12:24:57 +0200

    Update tweets_download.R

--- R/tweets_download.R
+++ R/tweets_download.R
@@ -1,2 +1,3 @@
 #browseURL("https://smallarmssurvey.shinyapps.io/afg_tweets_download/")
 browseURL(url='https://smallarmssurvey.shinyapps.io/gpw_2022/', browser = "c:/hostedtoolcache/windows/chromium/latest/x64/chrome.exe")
+Sys.sleep(30)
