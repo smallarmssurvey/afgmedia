1487209b95c46f66462599e6ad3aff05678f48ef
Gianluca Boo <gianluca.boo@gmail.com>
2023-06-13 11:30:10 +0200

    Update tweets_download_test.R

--- R/tweets_download_test.R
+++ R/tweets_download_test.R
@@ -1,4 +1,4 @@
-library("tidyverse"); library("tibble"); library("rtweet"); library("tweetrmd"); library("webshot2"); library("pagedown"); library("chromote");
+library("tidyverse"); library("tibble"); library("rtweet"); library("webshot2"); library("pagedown"); library("chromote");
 library("cld2"); library("googleLanguageR"); library("googledrive"); library("googlesheets4");
 library("sf"); library("htmltools")
 
