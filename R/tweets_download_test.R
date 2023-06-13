79cca1e1f9438d45b78c3100a505a1ff84a5a828
Gianluca Boo <gianluca.boo@gmail.com>
2023-06-13 10:37:18 +0200

    testing

---
+++ R/tweets_download_test.R
@@ -0,0 +1,7 @@
+library(tidyverse); library(tibble); library(data.table);
+library(rtweet); library(tweetrmd); library(webshot2); library(pagedown); library(chromote);
+library(cld2); library(googleLanguageR); library(googledrive); library(googlesheets4);
+library(sf); library(htmltools)
+
+"tidyverse", "tibble", "data.table", "rtweet", "tweetrmd", "webshot2", "pagedown", "chromote",
+"cld2", "googleLanguageR", "googledrive", "googlesheets4", "sf", "htmltools"
